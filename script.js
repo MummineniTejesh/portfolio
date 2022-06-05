ededda60f18d24a7dfd6944e90b068878d656b47
MummineniTejesh <tejeshmummineni@gmail.com>
2022-06-05 17:30:09 +0530

    home fix

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ function slider(){
     document.querySelector(".nav-links").classList.toggle("nav-active");
     document.querySelector(".burger").classList.toggle("toggle");
 }
+
+
+var array=["I am Tejesh"];
+var i=0;
+var speed=200;
+
+function type(){
+    document.querySelector(".name").innerHTML=array[0].substring(0,i)+"<span><b>|</b></span>";
+
+    if(i++!=array[0].length)
+    {
+        setTimeout(type,speed);
+    }
+}
+
+window.addEventListener("load",type);
